b79d417ad1042c4507bc7da7411eb66b094fbe67
Carlos Landaeta <calejandrolg99@gmail.com>
2022-07-21 02:34:27 -0400

    hotfix

--- lib/paciente/infraestructura/services/patient_services.dart
+++ lib/paciente/infraestructura/services/patient_services.dart
@@ -2,10 +2,9 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 class PatientService {
-  Future <dynamic> getPatient(String idPatient) async {
+  Future<dynamic> getPatient(String idPatient) async {
     var response = await http.get(Uri.parse(
-      "http://10.0.2.2:3000/api/paciente/user?id:649edad6-0795-4126-9398-f1728b7ef318"
-    )); // Cambiar por el url de los doctores
+        "http://10.0.2.2:3000/api/paciente/user?id=649edad6-0795-4126-9398-f1728b7ef318")); // Cambiar por el url de los doctores
     var jsonResponse = convert.jsonDecode(response.body);
 
     return jsonResponse;
