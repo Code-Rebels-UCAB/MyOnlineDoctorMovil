d0f933c8fc5ef9f10ff41b934f8aa96c9977aadc
Francogavidia <francogavidia027@gmail.com>
2022-08-03 06:04:02 -0400

    Se integro la autenticación en los servicios
    Se integro en los servicios la autenticación que se guarda gracias a la dependencia secure storage; los servicios que necesitan la seguridad para el id del paciente, además de adaptar los screens, con los services y el token de autenticación, ya que usaban datos hardcodeados.

--- lib/paciente/infraestructura/services/patient_services.dart
+++ lib/paciente/infraestructura/services/patient_services.dart
@@ -1,16 +1,30 @@
 import 'package:http/http.dart' as http;
+import 'package:myonlinedoctormovil/common/infraestructura/authentication/auth_service.dart';
 import 'dart:convert' as convert;
 
 import '../../../common/environment.dart';
+import '../../../common/infraestructura/authentication/ports/auth_service_abstract.dart';
 
 
 class PatientService {
-  Future<dynamic> getPatient(String idPatient) async {
-    var response = await http.get(Uri.parse(
-      "${SERVER_API}/api/paciente/info?id=$idPatient"
-    )); // Cambiar por el url de los doctores
-    var jsonResponse = convert.jsonDecode(response.body);
+  final AuthServiceAbstract authToken;
 
-    return jsonResponse;
+  PatientService(this.authToken);
+
+  Future<dynamic> getPatient() async {
+    try{
+      final token = await authToken.leerToken();
+      var response = await http.get(Uri.parse("${SERVER_API}/api/paciente/info"),
+          headers:{
+            "Content-type": "application/json",
+            'Authorization': 'Bearer $token',
+          });
+
+      var jsonResponse = convert.jsonDecode(response.body);
+
+      return jsonResponse;
+    } catch (e) {
+      throw Exception('Algo salio mal');
+    }
   }
 }
